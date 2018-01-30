73cfb3a5cb3bf1fdbfc363e7fd82c948214c79a1
Nivek H <nivomi@terribleideas.org>
2018-01-29 19:43:33 -0500

    Initial commit

---
+++ index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Set the above to match a virtualenv if you use one?
+import cgi
+
+print("Content-Type: text/html")    # HTML is following
+print()                             # blank line, end of headers
+
+form = cgi.FieldStorage()
+if "file" in form:
+    exit(0)
+    # TODO: process file
+else:
+    print("ass")
