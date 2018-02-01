1febe82768f834ff06155c005e1db3b0eca526e3
Nivek H <nivomi@terribleideas.org>
2018-02-01 13:18:22 -0500

    Implemented armoire and duplicate stack detection, and delivery to users.

--- RetainerInventoryParser.py
+++ RetainerInventoryParser.py
@@ -1,10 +1,12 @@
-import re
-
-
 class RetainerInventoryParser:
+    LANG_EN = "name_en"
+    LANG_JA = "name_ja"
+    LANG_DE = "name_de"
+    LANG_FR = "name_fr"
 
-    def __init__(self, logfile):
-
+    def __init__(self, logfile, lang):
+        self.lang = lang
+        assert self.lang is self.LANG_DE or lang is self.LANG_EN or lang is self.LANG_FR or lang is self.LANG_JA
         # structure...
         # list of retainers
         # each retainer has a name and a list of items
@@ -15,14 +17,22 @@ class RetainerInventoryParser:
 
         self.log = logfile
         self.retainers = []
-        self.armoire_items = []
-
+        self.armoire_alerts = []
+        #  each element is a dict with keys "retainer" and "item"
+        self.split_stack_alerts = []
+        #  each element is a tuple of ID, HQ bool, and list of owners
+        self.error_strings = []
         self.__parse_retainers()
+        self.__find_optimizations()
 
     def __parse_retainers(self):
+        import re
         resettable = False
         item_finder = re.compile(
-            '\d*?\|.*?\|00000060\|[0-9A-F]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|(?P<quantity>[0-9A-Fa-f]{8})\|(?P<item_id>[0-9A-Fa-f]{8})\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{4}(?P<quality>[0-9A-Fa-f]{4})')
+            '\d*?\|.*?\|00000060\|[0-9A-F]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|'
+            '[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|'
+            '(?P<quantity>[0-9A-Fa-f]{8})\|(?P<item_id>[0-9A-Fa-f]{8})\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]{8}\|[0-9A-Fa-f]'
+            '{8}\|[0-9A-Fa-f]{4}(?P<quality>[0-9A-Fa-f]{4})')
         retainer_name_finder = re.compile('\d*\|.*\|003d\|(?P<retainer_name>.*?)\|')
         current_retainer = {"Name": "(Failed to detect name!)", "Itemlist": []}
         current_itemlist = []
@@ -36,7 +46,13 @@ class RetainerInventoryParser:
                     self.retainers = []
                     current_retainer = {"Name": "(Failed to detect name!)", "Itemlist": []}
                     current_itemlist = []
-                current_itemlist.append((match.group('item_id'), match.group('quantity'), match.group('quality')))
+                if match.group('quality') == "8201":
+                    is_high_quality = True
+                elif match.group('quality') == "8200":
+                    is_high_quality = False
+                else:
+                    raise InvalidLogError("Anomalous item quality:" + match.group("quality"))
+                current_itemlist.append((match.group('item_id'), match.group('quantity'), is_high_quality))
             else:
                 if current_itemlist:  # if we don't have an itemlist, any matches would be unrelated retainer dialog
                     match = retainer_name_finder.match(line)
@@ -52,16 +68,45 @@ class RetainerInventoryParser:
             self.retainers.append(current_retainer)
         #  retainer inventory list parsing is done - now we search for optimizations
 
-    #  search for Armoire-compatible items
-    def __armoire_search(self):
+    def __find_optimizations(self):
+        import json
+        #  search for Armoire-compatible items
+        armoire_capable = []
+
+        for key, obj in json.load(open("data/armoire.json")).items():
+            armoire_capable.append(obj['item'])
+        itemlist_json = json.load(open("data/itemlist.json"))
+        itemid_owners = {}
+        self.armoire_alerts = []
         for retainer in self.retainers:
             name = retainer['Name']
             for item in retainer['Itemlist']:
-                exit(99)
-    #  search for same-item-on-different-retainer
+                if int(item[0]) in armoire_capable:
+                    self.armoire_alerts.append({'retainer': name, 'item': item})
+                itemid_owners.setdefault((item[0], item[2]), []).append((name, item[1]))
+        for item_uuid, owners in itemid_owners.items():
+            if len(owners) > 1:
+                if itemlist_json[item_uuid[0]]["stack_size"] > 1:
+                    self.split_stack_alerts.append((item_uuid[0], item_uuid[1], owners))
+
+        for alert in self.armoire_alerts:
+            self.error_strings.append("Item <strong>{item}</strong> on <strong>{retainer}</strong> can be placed in "
+                                      "the armoire.".format(retainer=alert['retainer'], item=itemlist_json[alert[
+                'item']][self.lang]))
+
+        for alert in self.split_stack_alerts:
+            ownerstring = ""
+            for owner in alert[2]:
+                ownerstring.join("{0} (x{1}), ".format(owner[0], owner[1]))
+            if alert[1]:
+                hq_string = "HQ"
+            else:
+                hq_string = "NQ"
+            self.error_strings.append(
+                "Item <strong>{item}({HQ})</strong> is on multiple retainers: <strong>{ownerstring}</strong>".format(
+                    item=itemlist_json[alert[1]][self.lang], HQ=hq_string, ownerstring=ownerstring))
 
 
 class InvalidLogError(Exception):
-    def __init__(self, message, line_number):
+    def __init__(self, message):
         self.message = message
-        self.line_number = line_number
--- process.py
+++ process.py
@@ -1,14 +1,36 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # Set the above to match a virtualenv if you use one?
 import cgi
+import cgitb
 
-print("Content-Type: text/html")    # HTML is following
-print()                             # blank line, end of headers
+from RetainerInventoryParser import RetainerInventoryParser
+
+cgitb.enable()
+print("Content-Type: text/html")  # HTML is following
+print()  # blank line, end of headers
 
 form = cgi.FieldStorage()
 if "file" in form:
-    exit(0)
-    # TODO: process file
+    if "language" in form:
+        if form["language"] == "de":
+            lang = RetainerInventoryParser.LANG_DE
+        elif form["language"] == "ja":
+            lang = RetainerInventoryParser.LANG_JA
+        elif form["language"] == "fr":
+            lang = RetainerInventoryParser.LANG_FR
+        else:
+            lang = RetainerInventoryParser.LANG_EN
+    else:
+        lang = RetainerInventoryParser.LANG_EN
+    parser = RetainerInventoryParser(form["file"].value, lang)
+
+    stringify = ""
+    if parser.error_strings:
+        for message in parser.error_strings:
+            stringify.join("<li>{0}</li>".format(message))
+    else:
+        stringify = "<li>No optimizations found, you seem pretty organized!</li>"
+    print(open("template/results.html").read().format(stringify))
 else:
-    page = open("template/error-nofile.html")
-    print(page)
+
+    print(open("template/error-nofile.html").read())
